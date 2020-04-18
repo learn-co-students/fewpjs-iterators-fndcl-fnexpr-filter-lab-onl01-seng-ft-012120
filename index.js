8feb97b5bc8260606c884da77f3d2ddd08e4325b
oliverfeher <oliver92.feher@gmail.com>
2020-04-18 01:03:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (arr, str) => {
+    return arr.filter( e => { return e.toUpperCase() === str.toUpperCase()})
+}
+
+const fuzzyMatch = (arr, str) => {
+    return arr.filter( e => { return e.substring(0, str.length).toUpperCase() === str.toUpperCase()})
+}
+
+const matchName = (arr, str) => {
+    return arr.filter( e=> { return e.name.toUpperCase() === str.toUpperCase()})
+}
