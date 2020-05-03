a907672074fda00a93dd8d9326f470cc5b47f55a
Ivana <ivana_pile@yahoo.com>
2020-05-02 22:38:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+// 1) returns all drivers that match the passed in name
+//returns matching drivers if case does not match but letters do
+//returns an empty array if there is no match
+function findMatching(array, string) {
+    return array.filter(item => item.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(item => item.slice(0, string.length).toLowerCase() === string.toLowerCase())
+}
+function matchName(array, string) {
+    return array.filter(item => item.name === string)
+}
