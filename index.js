a9323a7ca13cc8d07709231553e6f6ea800c300d
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-03 22:26:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(arr, name) {
+   let match =  arr.filter(n => {return n.toLowerCase() == name.toLowerCase() })
+    return match
+}
+
+function fuzzyMatch(arr, str) {
+    let fuzz = arr.filter(n => { return n.substring(0, str.length).toUpperCase() === str.toUpperCase()})
+    return fuzz
+}
+
+function matchName(arr, str) {
+    let match = arr.filter(n => {return n.name === str})
+    return match
+}
