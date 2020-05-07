4054ca689cdcc1cbe3afd225ab2e6056c0a71632
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-07 17:18:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(array, str) {
+  return array.filter(element => {return element.toLowerCase() === str.toLowerCase();});
+}
+
+function fuzzyMatch(array, str) {
+  return array.filter(element => {return element.toLowerCase().indexOf(str.toLowerCase()) === 0});
+}
+
+function matchName(array, str) {
+  return array.filter(element => {return element.name === str});
+}
