f70f442595bcd7e5b92553c3e0b245c86126d59e
xdundun <xdundun@gmail.com>
2020-05-18 11:16:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array,string) {
+  return array.filter(n => 
+     n.toLowerCase() === string.toLowerCase()
+  )
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(n => 
+    n.toLowerCase().charAt(0) === string.toLowerCase().charAt(0)
+  )
+}
+
+function matchName(array, string) {
+  return array.filter(n =>
+    n.name === string
+    )
+}
